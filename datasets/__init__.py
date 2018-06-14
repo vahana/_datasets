3e17a372d933ebc168452bea0b79b81e37367b61
vahan <vahan@gazelle.company>
2018-06-14 17:47:42 +0000

    * get_dataset is now more flexible to deal with ES and mongo

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -2,8 +2,23 @@ from pyramid.config import Configurator
 
 from prf.utils.dictset import dictset
 from .mongosets import *
+from .mongosets import get_dataset as mongo_get_dataset
 
 Settings = dictset()
+ES_BE_NAME = '__es'
+URL_BE_NAME = '__url'
+
+
+def get_dataset(ds, ns=None, define=False):
+    if isinstance(ds, dict):
+        if ds.get('backend') == ES_BE_NAME:
+            from prf.es import ES
+            return ES(ds.name)
+        else:
+            return mongo_get_dataset(ds.name, ns=ns, define=define)
+    else:
+        return mongo_get_dataset(ds, ns=ns, define=define)
+
 
 def main(global_config, **settings):
     global Settings
